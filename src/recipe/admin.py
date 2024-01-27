91e47168c9c023c87bcc34e2261ac15b16b4d1dd
Yura192c <fpm.mironchiYU@bsu.by>
2024-01-27 19:03:25 +0300

    Add Product, Recipe models

--- src/recipe/admin.py
+++ src/recipe/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Product, Recipe, RecipeProduct
 
-# Register your models here.
+
+class RecipeProductInline(admin.TabularInline):
+    model = RecipeProduct
+    extra = 1
+
+
+class RecipeAdmin(admin.ModelAdmin):
+    inlines = [RecipeProductInline]
+    list_display = ('id', 'name')
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'times_cooked')
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Recipe, RecipeAdmin)
--- src/recipe/models.py
+++ src/recipe/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    times_cooked = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=255)
+    products = models.ManyToManyField(Product, through='RecipeProduct')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Рецепт'
+        verbose_name_plural = 'Рецепты'
+
+
+class RecipeProduct(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name='Продукт')
+    weight = models.IntegerField(verbose_name='Вес (в граммах)')
+
+    def __str__(self):
+        return f'id продукта - {self.product.id}'
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['recipe', 'product'], name='unique_recipe_product')
+        ]
